9d282dc9dec53341102c214da1ee17480e091ee1
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-11-28 20:14:48 +0200

    made plots on matlab

--- plot_sorting_times.m
+++ plot_sorting_times.m
@@ -1,11 +1,25 @@
-%fileID = fopen('sorting_speeds.dat','r');
-
-%formatSpec = '%d %lf %lf %lf %lf';
-%sizeA = [5 Inf];
-
-%A = fscanf(fileID,formatSpec,sizeA);
-%fclose(fileID);
-
+%read data from c-code execution into one matrix
 A = dlmread('sorting_algorithms.txt');
 format shortG
-disp(A);
+%disp(A);
+
+%Store seperate units
+N = A(:,1);
+T_bubble = A(:,2);
+T_selection = A(:,3);
+T_insertion = A(:,4);
+T_merge = A(:,5);
+
+h = figure(1);
+clf;
+t_b = plot(N, T_bubble, 'm+');
+title("Sorting times depending on number of elements");
+xlabel("N");
+ylabel("t(sec)");
+
+hold on;
+t_s = plot(N, T_selection, 'bo');
+t_i = plot(N, T_insertion, 'gs');
+t_m = plot(N, T_merge, 'r*');
+legend({'bubble-sort','selection-sort','insertion-sort','merge-sort'}, 'Location','northwest');
+hold off;
